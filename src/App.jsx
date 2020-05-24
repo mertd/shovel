1809c58df92d95f1b0f4cca7ac7842c3e368e19b
mertd <mertd@users.noreply.github.com>
2020-05-24 16:02:24 +0900

    basic search works

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Fuse from "fuse.js";
 
-const fuseOptions = {}
+const fuseOptions = {
+  keys: [
+    "homepage",
+    "description",
+    "bin"
+  ]
+}
 
 function App() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState({});
+  const [results, setResults] = useState([]);
   const [manifests, setManifests] = useState([]);
   const [fuse, setFuse] = useState(new Fuse(manifests, fuseOptions));  
 
@@ -38,8 +44,8 @@ function App() {
       <main>
         <input value={search} onChange={(event) => setSearch(event.target.value)}/>
         <ul>
-          { results.map && results.map((result) => 
-            <li>test</li>
+          { results && results.map((result) => 
+            <li key={result.refIndex}>{result.item.description}</li>
           ) }
         </ul>
       </main>
