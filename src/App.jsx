6088d0af00e4f7f8a468e8babec9214dc4f662c8
mertd <mertd@users.noreply.github.com>
2020-05-26 20:12:25 +0900

    run prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Fuse from "fuse.js";
 
 const fuseOptions = {
   threshold: 0.2,
-  keys: [
-    "name",
-    "description"
-  ]
-}
+  keys: ["name", "description"],
+};
 
 function App() {
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
   const [manifests, setManifests] = useState([]);
-  const [fuse, setFuse] = useState(new Fuse(manifests, fuseOptions));  
+  const [fuse, setFuse] = useState(new Fuse(manifests, fuseOptions));
 
   async function getManifests() {
-    const response = await fetch("https://mertd.github.io/shovel-data/manifests.json");
+    const response = await fetch(
+      "https://mertd.github.io/shovel-data/manifests.json"
+    );
     const json = await response.json();
     setManifests(json);
   }
 
   useEffect(() => {
-    getManifests();   
+    getManifests();
   }, []);
 
   useEffect(() => {
     setFuse(new Fuse(manifests, fuseOptions));
-  }, [manifests])
+  }, [manifests]);
 
   useEffect(() => {
     const results = fuse.search(search);
     setResults(results);
     // eslint-disable-next-line
-  }, [search])
+  }, [search]);
 
   return (
     <div className="App">
       <header>
         <h1>Shovel</h1>
-        <h4>Search apps installable via <a href="https://scoop.sh" target="_blank" rel="noopener noreferrer">scoop</a></h4>
+        <h4>
+          Search apps installable via{" "}
+          <a href="https://scoop.sh" target="_blank" rel="noopener noreferrer">
+            scoop
+          </a>
+        </h4>
       </header>
       <main>
-        <input value={search} onChange={(event) => setSearch(event.target.value)}/>
+        <input
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
         <ul>
-          { results && results.map((result) => 
-            <li key={result.refIndex}>
-              <p><b><a href={result.item.homepage} target="_blank" rel="noopener noreferrer">{result.item.name}</a></b> {result.item.version} <i>{result.item.bucket}</i></p>
-              <p>{result.item.description}</p>
-              <p><code>scoop install {result.item.name}</code></p>
-            </li>
-          ) }
+          {results &&
+            results.map((result) => (
+              <li key={result.refIndex}>
+                <p>
+                  <b>
+                    <a
+                      href={result.item.homepage}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {result.item.name}
+                    </a>
+                  </b>{" "}
+                  {result.item.version} <i>{result.item.bucket}</i>
+                </p>
+                <p>{result.item.description}</p>
+                <p>
+                  <code>scoop install {result.item.name}</code>
+                </p>
+              </li>
+            ))}
         </ul>
       </main>
       <footer>
-        Shovel is <a href="https://github.com/mertd/shovel" target="_blank" rel="noopener noreferrer">open source</a>. 
-        Your use of this app is governed by the <a href="https://github.com/mertd/shovel/blob/master/LICENCE" target="_blank" rel="noopener noreferrer">MIT Licence</a>.
+        Shovel is{" "}
+        <a
+          href="https://github.com/mertd/shovel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open source
+        </a>
+        . Your use of this app is governed by the{" "}
+        <a
+          href="https://github.com/mertd/shovel/blob/master/LICENCE"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MIT Licence
+        </a>
+        .
       </footer>
     </div>
   );
