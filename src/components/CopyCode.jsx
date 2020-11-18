4e03c1c72252ef16b337cd758577c672fb042f18
mertd <mertd@users.noreply.github.com>
2020-11-18 23:51:20 +0900

    feedback

--- src/components/CopyCode.jsx
+++ src/components/CopyCode.jsx
@@ -1,15 +1,33 @@
-import { Code, Icon, Button } from "@chakra-ui/core";
+import { Code, Icon, Button, useToast } from "@chakra-ui/core";
 import React from "react";
 
 export default function CopyCode({ code }) {
+  const toast = useToast();
+
+  async function toClipboard() {
+    try {
+      await navigator.clipboard.writeText(code);
+      toast({
+        status: "success",
+        title: "Copied",
+        description: "You can paste this into your terminal now.",
+        duration: 3000,
+      });
+    } catch (error) {
+      toast({
+        status: "error",
+        title: "Error",
+        description:
+          "Copying to clipboard may not be supported in your browser.",
+        duration: 3000,
+      });
+    }
+  }
+
   return (
     <div>
       <Code>{code}</Code>
-      <Button
-        size="xs"
-        variant="outline"
-        onClick={() => navigator.clipboard.writeText(code)}
-      >
+      <Button size="xs" variant="outline" onClick={toClipboard}>
         <Icon name="copy" />
       </Button>
     </div>
