6cdcf84235f13bf3cb3a87e2442b8fb6f667572c
Mert Demir <mertd@users.noreply.github.com>
2020-11-19 00:04:53 +0900

    Copy to clipboard (#19)
    * custom code component
    
    * copy buttons
    
    * feedback
    
    * use IconButton
    
    * 0.5.0

---
+++ src/components/CopyCode.jsx
@@ -0,0 +1,38 @@
+import { Code, useToast, IconButton } from "@chakra-ui/core";
+import React from "react";
+
+export default function CopyCode({ code }) {
+  const toast = useToast();
+
+  async function toClipboard() {
+    try {
+      await navigator.clipboard.writeText(code);
+      toast({
+        status: "success",
+        title: "Copied",
+        description: "You can paste this into your terminal now.",
+        duration: 3000,
+      });
+    } catch (error) {
+      toast({
+        status: "error",
+        title: "Error",
+        description:
+          "Copying to clipboard may not be supported in your browser.",
+        duration: 3000,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Code>{code}</Code>{" "}
+      <IconButton
+        size="xs"
+        variant="outline"
+        onClick={toClipboard}
+        icon="copy"
+      />
+    </div>
+  );
+}
