6f7bc114424e6454c3903d460faed0661fa22961
mertd <mertd@users.noreply.github.com>
2020-11-18 22:50:33 +0900

    display version number in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import A from "./A";
 import { Box } from "@chakra-ui/core";
+import { version } from "../../package.json";
 
 function Footer(props) {
   return (
     <footer {...props}>
       <Box pt="2rem" pb="2rem" pl="20vw" pr="20vw" bg="white">
-        Shovel is <A href="https://github.com/mertd/shovel">open source</A>.
-        Your use of this app is governed by the{" "}
+        Shovel v{version} is{" "}
+        <A href="https://github.com/mertd/shovel">open source</A>. Your use of
+        this app is governed by the{" "}
         <A href="https://github.com/mertd/shovel/blob/master/LICENCE">
           MIT Licence
         </A>
