67b8e230f9afcf794becf895a659e468caf7f1f0
mertd <mertd@users.noreply.github.com>
2021-09-27 10:13:31 +0900

    use Link props

--- src/components/A.tsx
+++ src/components/A.tsx
@@ -1,9 +1,9 @@
-import { Link } from "@chakra-ui/react";
+import { Link, LinkProps } from "@chakra-ui/react";
 import React from "react";
 
-function A(props: React.ComponentPropsWithRef<"a">) {
+function A(props: LinkProps) {
   return (
-    <Link target="_blank" rel="noopener noreferrer" color="blue.600" {...props}>
+    <Link isExternal color="blue.600" {...props}>
       {props.children}
     </Link>
   );
