72029b32ad255ee8a7baccf462cd2b1602587a83
mertd <mertd@users.noreply.github.com>
2021-09-27 10:14:49 +0900

    doc

--- src/components/A.tsx
+++ src/components/A.tsx
@@ -1,6 +1,9 @@
 import { Link, LinkProps } from "@chakra-ui/react";
 import React from "react";
 
+/**
+ * External links. Use `Link` from react-router for internal links instead.
+ */
 function A(props: LinkProps) {
   return (
     <Link isExternal color="blue.600" {...props}>
