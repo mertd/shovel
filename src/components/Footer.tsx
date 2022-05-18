26c724a6414e3764cfb9a60963e7fcd2e9309020
Mert Demir <mertd@users.noreply.github.com>
2022-05-18 23:12:16 +0900

    Archival warning (#51)
    * toast on load
    
    * 0.10.4
    
    * mention archival
    
    * 10 seconds
    
    * wording

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import A from "./A";
-import { Box } from "@chakra-ui/react";
+import { Box, useToast } from "@chakra-ui/react";
 import { version } from "../../package.json";
 
 /**
  * Footer for any page
  */
 function Footer(props: React.ComponentPropsWithRef<"footer">) {
+  const toast = useToast();
+
+  useEffect(() => {
+    toast({
+      status: "warning",
+      title: "Archival Warning",
+      duration: 10000,
+      description: (
+        <>
+          The Scoop community chose an official web search. Please use it at{" "}
+          <b>
+            <a href="https://scoop.sh" target="_blank" rel="noreferrer">
+              scoop.sh
+            </a>
+          </b>
+          . Shovel will be archived and shut down after 2022. 🗄
+        </>
+      ),
+    });
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <footer {...props}>
       <Box pt="2rem" pb="2rem" pl="20vw" pr="20vw" bg="white">
